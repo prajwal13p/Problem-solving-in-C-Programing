412a29028fb51c6efb36acf34e04091b216ca5b8
Prajwal P <108621297+prajwal13p@users.noreply.github.com>
2024-12-24 18:27:20 +0530

    Update merging_two strings alternatly.c

--- merging_two strings alternatly.c
+++ merging_two strings alternatly.c
@@ -1,27 +1,37 @@
 #include<stdio.h>
-int count(int n);
-int main(void){
-    int i;
-    for(i = 1;i<1000;i++){
-        int n,c,sum;
-        c = count(i);
-        sum = 0;
-        n = i;
-        while(n>0){
-            sum += (int)pow(n%10,c);
-            n /= 10;
-        }
-        if(i == sum)
-        printf("%d",i);
-    }
-    return 0;
-}
+#include<string.h>
+#include<stdlib.h>
+char *merge_strings(char *word1,char *word2 );
 
-int count(int n){
-    int c = 0;
-    while(n%10 != 0){
-        c++;
-        n /= 10;
+int main(void){
+    char word1[] = "prajwal";
+    char word2[] = "programming";
+
+    char *word = merge_strings(word1,word2);
+    printf("%s",word);
+    free(word);
+    return 0;
+
+}
+char *merge_strings(char *word1,char *word2 ){
+    int total1 = 0, total2 = 0,total = 0,i = 0 , index = 0;
+
+    while(word1[total1] != '\0') total1++;
+    while(word2[total2] != '\0') total2++;
+
+    total = total1 + total2 + 1;
+
+    char *word = malloc(total * sizeof(char));
+
+    while(i<total1 || i < total2){
+        if(i < total1){
+            word[index++] = word1[i];
+        }
+        if(i<total2){
+            word[index++] = word2[i];
+        }
+        i++;
     }
-    return c;
+    word[index] = '\0';
+    return word;
 }
